50e1c7d81d6b54f00e835071c932bb1c8b43549f
Relax <nikitafon2005@gmail.com>
2025-04-21 10:30:15 +0300

    add new foto

--- js/meat.js
+++ js/meat.js
@@ -5,22 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Инициализация страницы продуктов
     ProductPage.init('selectedMeatType', 'product__card');
 
-    // Конфигурация стрелок для страницы мяса
-    const arrowConfig = [
-        {
-            selector: '.nav-arrow--to-chicken',
-            targetCategory: 'chicken'
-        },
-        {
-            selector: '.nav-arrow--to-categories',
-            targetPage: 'categories.html'
-        },
-        {
-            selector: '.nav-arrow--to-vegetables',
-            targetPage: 'vegetables.html'
-        }
-    ];
-
     // Инициализация навигационных стрелок
     ProductPage.initArrows(arrowConfig);
 }); 
